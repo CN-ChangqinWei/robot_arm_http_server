5b94d51f9d97656ee67f65539886963fcd1d4ddf
ChangqinWei <2296195685@qq.com>
2026-03-10 10:12:41 +0800

    add: forward rest交付层fin

--- internal/rest/forward.go
+++ internal/rest/forward.go
@@ -1,1 +1,45 @@
 package rest
+
+import (
+	"context"
+	"net/http"
+
+	"github.com/bxcodec/go-clean-arch/domain"
+	"github.com/labstack/echo/v4"
+)
+
+type ForwardService interface {
+	GetTopics(ctx context.Context) (res []domain.Forward, err error)
+	GetTopicInfo(ctx context.Context, topic string) (res domain.Forward, err error)
+}
+
+type ForwardHandler struct {
+	Service ForwardService
+}
+
+func NewForwardHandler(e *echo.Echo, svc ForwardService) {
+	handler := &ForwardHandler{
+		Service: svc,
+	}
+	e.GET("/forward/topics", handler.GetTopics)
+	e.GET("/forward/info/:topic", handler.GetTopicInfo)
+}
+
+func (f *ForwardHandler) GetTopics(c echo.Context) (err error) {
+	ctx := c.Request().Context()
+	res, err := f.Service.GetTopics(ctx)
+	if err != nil {
+		return c.JSON(getStatusCode(err), ResponseError{Message: err.Error()})
+	}
+	return c.JSON(http.StatusOK, res)
+}
+func (f *ForwardHandler) GetTopicInfo(c echo.Context) (err error) {
+	topic := c.Param("topic")
+	ctx := c.Request().Context()
+	res, err := f.Service.GetTopicInfo(ctx, topic)
+	if nil != err {
+		return c.JSON(getStatusCode(err), ResponseError{Message: err.Error()})
+	}
+	return c.JSON(http.StatusOK, res)
+
+}
